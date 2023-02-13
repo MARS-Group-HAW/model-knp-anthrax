c5bf8175b74d97a33a551679ebfce561a03131e3
Jonathan Ströbele <mail@jonathanstroebele.de>
2023-02-13 21:57:06 +0100

    export anthrqax layer as GeoJSON

--- KNPAnthrax/Model/AnthraxLayer.cs
+++ KNPAnthrax/Model/AnthraxLayer.cs
@@ -1,8 +1,72 @@
+using System.IO;
 using Mars.Components.Layers;
+using Mars.Interfaces.Layers;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
 
 namespace KNPAnthrax.Model;
 
-public class AnthraxLayer : RasterLayer
+public class AnthraxLayer : RasterLayer, ISteppedActiveLayer
 {
+    public void PreTick()
+    {
+    }
     
+    public void Tick()
+    {
+    }
+    
+    public void PostTick()
+    {
+        if (GetCurrentTick() == Context.MaxTicks)
+        {
+            ToGeoJSON();
+        }
+    }
+    
+    public void ToGeoJSON()
+    {
+        var featureCollection = new FeatureCollection();
+        var gf = NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(4326);
+
+        for (var x = 0; x < Width; x++)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                var value = this[x, y];
+                
+                if (value == 0)
+                {
+                    continue;
+                }
+
+                if (value == NoDataValue)
+                {
+                    continue;
+                }
+                
+                // p4      p3
+                // + ---- +
+                // |      |
+                // |      |
+                // + ---- + 
+                // p1      p2
+                var p = gf.CreatePolygon(new[] {
+                    new Coordinate(LowerLeft.X + CellWidth * x, LowerLeft.Y + CellHeight * y), // p1
+                    new Coordinate(LowerLeft.X + CellWidth * x + CellWidth, LowerLeft.Y + CellHeight * y), // p2
+                    new Coordinate(LowerLeft.X + CellWidth * x + CellWidth, LowerLeft.Y + CellHeight * y + CellHeight), // p3
+                    new Coordinate( LowerLeft.X + CellWidth * x, LowerLeft.Y + CellHeight * y + CellHeight), // p4
+                    new Coordinate(LowerLeft.X + CellWidth * x, LowerLeft.Y + CellHeight * y), // p1
+                });
+                var at = new AttributesTable();
+                at.Add("density", value);
+                featureCollection.Add(new Feature(p, at));
+            }
+        }
+        
+        var write = new GeoJsonWriter().Write(featureCollection);
+        File.WriteAllText($"{this.GetType().Name}.geojson", write);
+    }
+
 }
