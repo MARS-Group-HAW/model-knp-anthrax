9b83cd0c3192535fdca2d715fa6e1e08c74c5642
Jonathan Ströbele <mail@jonathanstroebele.de>
2023-02-13 17:16:39 +0100

    feat: use vector based perimter layer so we can use the unuion of the provided landtype shp file

--- KNPAnthrax/Model/Perimeter.cs
+++ KNPAnthrax/Model/Perimeter.cs
@@ -1,12 +1,15 @@
+using System;
+using System.Linq;
 using Mars.Components.Layers;
-using Mars.Interfaces.Environments;
+using NetTopologySuite.Geometries;
+using Position = Mars.Interfaces.Environments.Position;
 
 namespace KNPAnthrax.Model;
 
 /// <summary>
 ///     A raster layer that ingests an .asc raster file with the walkable area by the agents.
 /// </summary>
-public class Perimeter : RasterLayer
+public class Perimeter : VectorLayer
 {
     /// <summary>
     ///     Checks for the coordinate whether this point is inside the perimeter (the defined polygon).
@@ -17,9 +20,12 @@ public class Perimeter : RasterLayer
     /// </returns>
     public bool IsPointInside(Position coordinate)
     {
+        var g = new Point(coordinate.X, coordinate.Y);
+        return Features.First().VectorStructured.Geometry.Contains(g);
+        
         // First, check if the coordinate is inside the area of the .asc file.
         // The comparison with "1" ensures that it's not a "non-walkable" cell (the area outside of our polygon).
         // It's the NoData value that was set previously in QGIS.
-        return Extent.Contains(coordinate.X, coordinate.Y) && GetValue(coordinate) != NoDataValue.GetValueOrDefault();
+        //return Extent.Contains(coordinate.X, coordinate.Y) && GetValue(coordinate) != NoDataValue.GetValueOrDefault();
     }
 }
